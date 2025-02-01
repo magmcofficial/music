cca165c0df0dc9310e119341a9473cffcef9fdb4
SHIDAN V <111380045+ytshidan50@users.noreply.github.com>
2025-02-02 01:56:16 +0530

    Add files via upload

---
+++ UI/icons/musicicons.js
@@ -0,0 +1,20 @@
+const musicIcons = {
+    footerIcon: 'https://cdn.discordapp.com/attachments/1281974737102442619/1300428560175206442/km_yts0_360p_12f_20241028_022053.gif?ex=6720cdf9&is=671f7c79&hm=b1254179c4ee0f53c48dae286954914f6b96993e487c2b8a4468392903e18170&', 
+    correctIcon: 'https://cdn.discordapp.com/emojis/820971058089820200.gif', 
+    playerIcon: 'https://cdn.discordapp.com/emojis/992051186188435467.gif',
+    pauseresumeIcon:'https://cdn.discordapp.com/emojis/982935235807313971.gif',
+    loopIcon: 'https://cdn.discordapp.com/emojis/749272851529334795.gif',
+    beatsIcon: 'https://cdn.discordapp.com/emojis/928310693416009828.gif',
+    beats2Icon : 'https://cdn.discordapp.com/emojis/953413565400879165.gif',
+    alertIcon : 'https://cdn.discordapp.com/emojis/996431685358981201.gif',
+    skipIcon: 'https://cdn.discordapp.com/emojis/938388856095514654.gif',
+    stopIcon: 'https://cdn.discordapp.com/emojis/1021628438441902100.gif',
+    volumeIcon: 'https://cdn.discordapp.com/emojis/1040824501711159397.gif',
+    playlistIcon : 'https://cdn.discordapp.com/emojis/1096444591982522498.gif',
+    heartIcon : 'https://cdn.discordapp.com/emojis/900257798003240961.gif',
+    pingIcon : 'https://cdn.discordapp.com/emojis/923089856752664576.gif',
+    CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+    MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+};
+
+module.exports = musicIcons;
