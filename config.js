42522dd7ce9d5f8fd7dc5c4a4aa7ceb0844fec82
MAG Official <magmcofficial@gmail.com>
2025-02-02 04:38:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-     {
+  /*   {
       name: "Koi Node V4",
       password: "prplmoe.me",
       host: "lavav4.prplmoe.me",
@@ -26,6 +26,20 @@ module.exports = {
       password: "NAIGLAVA-dash.techbyte.host",
       host: "lavahatry4.techbyte.host",
       port: 3000,
+      secure: false
+    } */
+    {
+      name: "Catfein ID",
+      password: "catfein",
+      host: "lava.catfein.com",
+      port: 4000,
+      secure: false
+    },
+    { 
+      name: "saher",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
       secure: false
     }
   ]
